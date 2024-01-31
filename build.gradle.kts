256a69ed3e9b071e6a4dd596c24765f62332de9d
Martin Vysny <mavi@vaadin.com>
2024-01-31 13:21:35 +0200

    Gradle: take version catalog into use

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
+import com.vaadin.gradle.getBooleanProperty
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     java
     application
-    id("com.vaadin")
+    alias(libs.plugins.vaadin)
 }
 
 defaultTasks("clean", "build")
@@ -14,34 +15,33 @@ repositories {
 
 dependencies {
     // Vaadin
-    implementation("com.vaadin:vaadin-core:${properties["vaadinVersion"]}") {
-        afterEvaluate {
-            if (vaadin.productionMode.get()) {
-                exclude(module = "vaadin-dev")
-            }
+    implementation(libs.vaadin.core) {
+        // https://github.com/vaadin/flow/issues/18572
+        if (vaadin.productionMode.map { v -> getBooleanProperty("vaadin.productionMode") ?: v }.get()) {
+            exclude(module = "vaadin-dev")
         }
     }
 
     // Vaadin-Boot
-    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:12.2")
+    implementation(libs.vaadinboot)
 
-    implementation("org.jetbrains:annotations:24.0.1")
+    implementation(libs.jetbrains.annotations)
     implementation("com.github.mvysny.vaadin-simple-security:vaadin-simple-security:1.0")
 
     // db
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    implementation("com.h2database:h2:2.2.224")
+    implementation(libs.hikaricp)
+    implementation(libs.flyway)
+    implementation(libs.h2)
     implementation("com.gitlab.mvysny.jdbiormvaadin:jdbi-orm-vaadin:1.1")
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation(libs.slf4j.simple)
 
     // Fast Vaadin unit-testing with Karibu-Testing: https://github.com/mvysny/karibu-testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.kaributesting)
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform)
 }
 
 java {
@@ -59,4 +59,3 @@ tasks.withType<Test> {
 application {
     mainClass.set("com.example.security.Main")
 }
-
